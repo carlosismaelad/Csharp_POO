8bb61ea74b2ff9991aba6ace3c1d7ff3ca2a07bd
carlosismaelad <carlosismaelad@outlook.com>
2023-08-03 18:50:55 -0300

    Estudando membros e métodos estáticos

---
+++ StaticMembers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+namespace StaticMembers
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args){
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunference(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+
+        static double Circunference(double r){
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r){
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ StaticMembers/README.md
@@ -0,0 +1,18 @@
+## Membros estáticos
+
+- Também chamados membros de classe
+(Em oposição a membros de instância)
+
+- São membros que fazem sentido independentemente de objetos. Não
+precisam de objeto para serem chamados. São chamados a partir do 
+próprio nome da classe.
+
+- Aplicações comuns:
+1 - Classes utilitárias -> "Math.Sqrt()", por exemplo;
+2 - Declaração de constantes
+
+- Uma classe que possui somente membros estáticos, pode ser uma classe estática também.
+Está classe não pode ser instanciada.
+
+
+
