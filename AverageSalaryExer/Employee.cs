7265f7cf3252ef6f8143a2969c7cfdd69b5c4221
carlosismaelad <carlosismaelad@outlook.com>
2023-08-03 11:48:08 -0300

    Correção na Classe Employee

--- AverageSalaryExer/Employee.cs
+++ AverageSalaryExer/Employee.cs
@@ -5,7 +5,7 @@ namespace ExerClassesAverageSalaries
         public string Name { get; set; }
         public double Salary { get; set; }
 
-        public static double CalcAverageSalary(Employee emp1, Employee emp2){
+        public double CalcAverageSalary(Employee emp1, Employee emp2){
             return (emp1.Salary + emp2.Salary) / 2;
         }
     }
