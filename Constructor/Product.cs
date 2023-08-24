4a95e301290a11b4321ddbdcdd78d966ff1ee87b
carlosismaelad <carlosismaelad@outlook.com>
2023-08-24 00:30:58 -0300

    Construtores e sobrecarga

---
+++ Constructor/Product.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+
+namespace Constructor
+{
+    public class Product
+    {
+        public string Name;
+        public double Price;
+        public int InStock;
+
+        public Product(string name, double price, int inStock){
+            this.Name = name;
+            this.Price = price;
+            this.InStock = inStock;
+        }
+
+        public double TotalValueInStock(){
+            return Price * InStock;
+        }
+
+
+        public void AddToStock(int quantity){
+            this.InStock += quantity;
+        }
+
+        public void RemoveFromStock(int quantity){
+            this.InStock -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return  Name 
+                    + ", R$ " 
+                    + Price.ToString("F2", CultureInfo.InvariantCulture) 
+                    + ", " 
+                    + InStock 
+                    + " unidades, Total: R$ " 
+                    + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
