26fa58a0b6ddbc65f6af6008aa4f4da0fafd36bd
carlosismaelad <carlosismaelad@outlook.com>
2023-08-28 11:21:27 -0300

    Exercício 1 sobre vetores usando classes

---
+++ ExerVetoresProducts/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace ExerVetoresProducts
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ ExerVetoresProducts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerVetoresProducts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Quantidade de produtos que serão cadastrados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            double sum = 0.0;
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.Write($"Nome do produto {i + 1}: ");
+                string name = Console.ReadLine();
+                Console.Write($"Preço do produto {i + 1}: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+                sum += vect[i].Price;
+            }
+
+            double averagePrice = sum / vect.Length;
+            Console.WriteLine($"Preço médio dos produtos: ${averagePrice.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
+
+
