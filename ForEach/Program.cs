1e165cab829acabb79d153a2a9291334b85f67a9
carlosismaelad <carlosismaelad@outlook.com>
2023-08-29 12:23:26 -0300

    Introdução ao laço foreach

---
+++ ForEach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "André", "Marcelo", "Paulo" };
+
+            foreach (string nome in vect)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
